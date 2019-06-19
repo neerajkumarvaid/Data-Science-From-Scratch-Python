38fe14dad28db1ec8264aed8ec9a39bb80f33e5d
Neeraj Kumar Vaid <neeraj.kumar.iitg@gmail.com>
2019-06-18 23:19:05 -0400

    Add files via upload

---
+++ 4_matrix_operations.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 17 12:23:40 2019
+
+@author: vaid-computer
+"""
+
+from typing import List, Tuple, Callable
+
+Matrix = List[List[float]]
+
+def shape(A: Matrix) -> Tuple[int, int]:
+    """Returns the size of a matrix"""
+    num_rows = len(A) # number of rows in a matrix
+    num_cols = len(A[0]) if A else 0 # number of elements in first row
+    
+    return num_rows, num_cols
+
+def make_matrix(num_rows: int, 
+                num_cols: int, 
+                entry_fn: Callable[[int,int], float]) -> Matrix:
+    """Creates a matrix given the number of rows and columns,
+    and a generator function."""
+    return [[entry_fn(i,j) for j in range(num_cols)] for i in range(num_rows)]
+    
+def identity_matrix(n: int) -> Matrix:
+    """Creates an nxn identity matrix"""
+    return make_matrix(n,n,lambda i,j:1 if i == j else 0)
+
+print(identity_matrix(5))
