c2686f2b92626a295b9bb6c41171c72e6ff1677c
Neeraj Kumar Vaid <neeraj.kumar.iitg@gmail.com>
2019-06-26 21:55:20 -0400

    Create 6_probability.py
    This code illustrates how to do basic porbability operations in Python.

---
+++ 6_probability.py
@@ -0,0 +1,6 @@
+"""
+Created on Mon Jun 26 00:55:07 2019
+@author: Neeraj
+Description: This code illustrates how to do basic porbability operations in Python. 
+Reference: Chapter 6 : Probability
+"""
