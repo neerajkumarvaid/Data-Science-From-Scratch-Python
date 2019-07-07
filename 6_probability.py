ba7a0fff48a68b47ca09969013f3d4de2ff2f88b
Neeraj Kumar Vaid <neeraj.kumar.iitg@gmail.com>
2019-07-07 06:05:07 -0400

    Update 6_probability.py
    This code illustrates how to do basic porbability operations in Python.

--- 6_probability.py
+++ 6_probability.py
@@ -45,13 +45,6 @@ def uniform_cdf(x: float) -> float:
   elif x < 1: return x
   else: return 1
 
-# Create cdf of a uniform random variable
-def uniform_cdf(x: float) -> float:
-  if x <= 0: return 0 
-  elif x < 1: return x
-  else: return 1
-  return 0 if x <= 0
-
 # Plot uniform pdf
 import numpy as np
 x = np.linspace(-0.5,1.5,21)
@@ -98,6 +91,7 @@ plt.plot(x,[normal_cdf(i,0,1) for i in x], '-', label ='mu = 0, sigma = 1')
 plt.plot(x,[normal_cdf(i,0,2) for i in x], '--', label ='mu = 0, sigma = 2')
 plt.plot(x,[normal_cdf(i,1,1) for i in x], '-.', label ='mu = 1, sigma = 1')
 
+
 # Inverse of normal distribution
 def inverse_normal_cdf(p: float, 
                        mu: float = 0, 
@@ -127,3 +121,35 @@ def inverse_normal_cdf(p: float,
 
 print(inverse_normal_cdf(0.3))      
         
+import random
+
+def bernoulli_trial(p: float) -> int:
+    """Returns 1 with probability p and 0 with probability 1-p"""
+    return 1 if random.random() < p else 0
+
+def binomial(n: int, p: float) -> int:
+    """Returns the sum of n bernoulli(p) trials"""
+    return sum(bernoulli_trial(p) for _ in range(n))
+
+from collections import Counter
+
+def binomial_histogram(p: float, n: int, num_points: int) -> None:
+    """Picks points from a Binomial(n, p) and plots their histogram"""
+    data = [binomial(n, p) for _ in range(num_points)]
+
+    # use a bar chart to show the actual binomial samples
+    histogram = Counter(data)
+    plt.bar([x - 0.4 for x in histogram.keys()],
+            [v / num_points for v in histogram.values()],
+            0.8,
+            color='0.75')
+
+    mu = p * n
+    sigma = sqrt(n * p * (1 - p))
+
+    # use a line chart to show the normal approximation
+    xs = range(min(data), max(data) + 1)
+    ys = [normal_cdf(i + 0.5, mu, sigma) - normal_cdf(i - 0.5, mu, sigma)
+          for i in xs]
+    plt.plot(xs,ys)
+    plt.title("Binomial Distribution vs. Normal Approximation")
