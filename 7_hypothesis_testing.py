cb2930f9df60182c5f4ef2e0826b2b5ce23acfe0
Neeraj Kumar Vaid <neeraj.kumar.iitg@gmail.com>
2019-07-07 20:18:47 -0400

    Update 7_hypothesis_testing.py

--- 7_hypothesis_testing.py
+++ 7_hypothesis_testing.py
@@ -16,3 +16,23 @@ def normal_approximation_binomial(n: int, p: float) -> Tuple[float,float]:
     """Estimates mu and sigma for a specified p and n"""
     mu = n*p
     sigma = math.sqrt(n*p*(1-p))
+    
+#It's above threshold if it's not below the threshold
+def normal_probability_above(lo: float,
+                             mu: float = 0,
+                             sigma: float = 1) -> float:
+    return 1 - normal_cdf(lo, mu, sigma)
+
+#It's in between if it is less than hi but greater than lo
+def normal_probability_between(lo: float,
+                               hi: float,
+                               mu: float = 0,
+                               sigma: float = 1) -> float:
+    return normal_cdf(hi, mu, sigma) - normal_cdf(lo, mu, sigma)
+
+# It's outside if not in between
+def normal_probability_outside(lo: float,
+                               hi: float,
+                               mu: float = 0,
+                               sigma: float = 1) -> float:
+    return 1 - normal_probability_between(lo, high, mu, sigma)
