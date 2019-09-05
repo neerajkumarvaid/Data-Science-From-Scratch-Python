5504c94fcdace3ffdeec30f0464315100cda1312
Neeraj Kumar Vaid <neeraj.kumar.iitg@gmail.com>
2019-09-05 15:59:47 -0400

    Update 5_statistics.py

--- 5_statistics.py
+++ 5_statistics.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
 """
-Spyder Editor
-
-This is a temporary script file.
+Created on Mon Jun 17 00:55:07 2019
+@author: Neeraj
+Description: This code computes basic statstics including mean, variance, covariance, correlation etc. from the data.
+vectors in this code.
+Reference: Chapter 4 : Linear Algebra 
 """
+
 
 num_friends = [100.0,49,41,40,25,21,21,19,19,18,18,16,15,15,15,15,14,14,13,13,13,13,12,12,11,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,8,8,8,8,8,8,8,8,8,8,8,8,8,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
 
