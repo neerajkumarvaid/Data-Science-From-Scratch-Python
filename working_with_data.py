08dd320344b7fe4b1d1e52ca166f1507032281f7
Neeraj Kumar Vaid <neeraj.kumar.iitg@gmail.com>
2019-09-09 16:33:27 -0400

    Update working_with_data.py

--- working_with_data.py
+++ working_with_data.py
@@ -251,3 +251,24 @@ def rescale(data: List[Vector]) -> List[Vector]:
             
     return rescaled
 
+import tqdm
+
+for i in tqdm.tqdm(range(100)):
+    _ = [random.random for _ in range(10000000)]
+    
+from typing import List
+
+def primes_up_to(n: int) -> List[int]:
+    primes = [2]
+    
+    with tqdm.trange(3,n) as t:
+        for i in t:
+            # i is prime if no smaller prime divides it
+            i_is_prime = not any(i % p == 0 for p in primes)
+            if i_is_prime:
+                primes.append(i)
+                
+            t.set_description(f"{len(primes)} primes")
+    return primes
+
+my_primes = primes_up_to(1000)
