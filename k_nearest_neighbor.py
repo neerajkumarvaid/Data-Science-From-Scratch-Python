974d575bd575a2035215a5eb6af46fa3a491933c
Neeraj Kumar Vaid <neeraj.kumar.iitg@gmail.com>
2019-09-13 23:34:06 -0400

    Update k_nearest_neighbor.py

--- k_nearest_neighbor.py
+++ k_nearest_neighbor.py
@@ -20,3 +20,23 @@ def majority_vote(labels: List[str]) -> str:
         return majority_vote(labels[:-1]) # try again without the farthest
     
 assert majority_vote(['a','b','c','b','a']) == 'b'
+
+from typing import NamedTuple
+from vector_operations import Vector, distance;
+
+class LabeledPoint(NamedTuple):
+    point: Vector
+    label: str
+        
+def knn_classify(k: int,
+                labeled_points: List[Vector],
+                new_point: Vector) -> str:
+    # Order the labeled points from nearest to farthest
+    by_distance = sorted(labeled_points, 
+                         key = lambda lp: distance(lp.point, new_point))
+    
+    # Find the labels for the k closest
+    k_nearest_labels = [lp.label for lp in by_distance[:k]]
+    
+    # and let them vote
+    return majority_vote(k_nearest_labels)
