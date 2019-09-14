17e09eb22fe16c86f392288c389abf5b5d2922f5
Neeraj Kumar Vaid <neeraj.kumar.iitg@gmail.com>
2019-09-13 23:33:45 -0400

    Update k_nearest_neighbor.py

--- k_nearest_neighbor.py
+++ k_nearest_neighbor.py
@@ -7,3 +7,16 @@ def raw_majority_votes(labels: List[str]) -> str:
     return winner
 
 assert raw_majority_votes(['a','b','c','d','b']) == 'b'
+
+def majority_vote(labels: List[str]) -> str:
+    """Assumes that the lables are ordered from nearest to farthest"""
+    vote_counts = Counter(labels)
+    winner, winner_count = vote_counts.most_common(1)[0]
+    num_winners = len([count for count in vote_counts.values()
+                      if count == winner_count])
+    if num_winners == 1:
+        return winner
+    else:
+        return majority_vote(labels[:-1]) # try again without the farthest
+    
+assert majority_vote(['a','b','c','b','a']) == 'b'
