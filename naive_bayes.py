92ef50cda7a3b8cd6a7a400d958aebb0aa539708
Neeraj Kumar Vaid <neeraj.kumar.iitg@gmail.com>
2019-09-15 02:00:54 -0400

    Update naive_bayes.py

--- naive_bayes.py
+++ naive_bayes.py
@@ -116,3 +116,26 @@ assert model.token_ham_counts == {"ham": 2, "rules": 1, "hello": 1}
 
 text = "hello spam"
 model.predict(text)
+
+from io import BytesIO # So we can treat bytes as a file
+import requests # To download files which
+import tarfile # are in .tar.bz format
+
+BASE_URL = "https://spamassassin.apache.org/old/publiccorpus"
+
+FILES = ["20021010_easy_ham.tar.bz2",
+        "20021010_hard_ham.tar.bz2",
+        "20021010_spam.tar.bz2"]
+
+OUTPUT_DIR = "spam_data"
+
+for filename in FILES:
+    # Use requests to get the file contents at each URL
+    content = requests.get(f"{BASE_URL}/{filename}").content
+    
+    # Wrap the in-memory bytes so we can use them as a file
+    fin = BytesIO(content)
+    
+    # And extract all the files to the output directory
+    with tarfile.open(fileobj= fin, mode = 'r:bz2') as tf:
+        tf.extractall(OUTPUT_DIR)
