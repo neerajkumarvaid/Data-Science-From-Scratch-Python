05ec90b63ae3179261f698747ecb2bd0550fe1d8
Neeraj Kumar Vaid <neeraj.kumar.iitg@gmail.com>
2019-09-15 02:00:34 -0400

    Update naive_bayes.py

--- naive_bayes.py
+++ naive_bayes.py
@@ -106,3 +106,13 @@ messages = [Message("spam rules", is_spam = True),
 
 model = NaiveBayesClassifier(k = 0.5)
 model.train(messages)
+
+# Let's check if our model got all tokens right
+assert model.tokens  == {"spam","ham","rules","hello"}
+assert model.spam_messages == 1
+assert model.ham_messages == 2
+assert model.token_spam_counts == {"spam": 1, "rules": 1}
+assert model.token_ham_counts == {"ham": 2, "rules": 1, "hello": 1}
+
+text = "hello spam"
+model.predict(text)
