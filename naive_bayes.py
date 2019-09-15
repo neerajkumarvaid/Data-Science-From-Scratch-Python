ee8c5ea47deafd4e8bee670301300a31f9bdde62
Neeraj Kumar Vaid <neeraj.kumar.iitg@gmail.com>
2019-09-14 23:24:26 -0400

    Update naive_bayes.py

--- naive_bayes.py
+++ naive_bayes.py
@@ -1,1 +1,11 @@
 
+from typing import Set
+import re
+
+def tokenize(text: str) -> Set[str]:
+    text = text.lower() # lowercase all the letter in text
+    all_words = re.findall("[a-z0-9']+", text) # find all words containing alphabets, number and apostrophies
+    # remove duplicates / extract unique words
+    return set(all_words)
+
+print(tokenize('Data Science is a science.'))
