b1fa93b12f83aa19b4162eccbb4d6dc280647110
Neeraj Kumar Vaid <neeraj.kumar.iitg@gmail.com>
2019-09-17 00:58:23 -0400

    Update multiple_regression.py

--- multiple_regression.py
+++ multiple_regression.py
@@ -6,3 +6,20 @@ def predict(x: Vector, beta: Vector) -> float:
 
 predict([1,49,4,0],[3,5,2,4])
 
+def error(x: Vector, y: float, beta: Vector) -> float:
+    return predict(x, beta) -y
+
+def squared_error(x: Vector, y: float, beta: Vector) -> float:
+    return error(x, y, beta) ** 2
+
+x = [1,2,3]
+y = 30
+beta = [4,4,4]
+print(error(x,y,beta))
+print(squared_error(x,y,beta))
+
+def sqerror_gradient(x: Vector, y: float, beta: Vector) -> Vector:
+    return [2 * error(x,y,beta) * x_i for x_i in x]
+
+sqerror_gradient(x,y,beta)
+
