e56a1cde4ddc38739cf50a21baa85680d3f8eb10
Neeraj Kumar Vaid <neeraj.kumar.iitg@gmail.com>
2019-09-21 23:56:43 -0400

    Update multiple_regression.py

--- multiple_regression.py
+++ multiple_regression.py
@@ -106,3 +106,20 @@ def bootstrap_statistic(data: List[X],
                        num_samples: int) -> List[Stat]:
     """Evaluates stats_fn on num_samples bootstrap samples from data"""
     return [stats_fn(bootstrap_sample(data)) for _ in range(num_samples)]
+
+import random
+# 101 points all very close to 100
+close_to_100 = [99.5 + random.random() for _ in range(101)]
+# 101 points, 50 of them near 0 and 50 of them near 100
+far_from_100 = ([99.5 + random.random()] +
+                [random.random() for _ in range(50)] +
+                [200 + random.random() for _ in range(50)])
+
+from statistics import median, standard_deviation;
+
+medians_close = bootstrap_statistic(close_to_100, median, 100)
+medians_far = bootstrap_statistic(far_from_100, median, 100)
+std_close = standard_deviation(medians_close)
+std_far = standard_deviation(medians_far)
+
+print(f"std_medians_close = {std_close}, std_medians_far = {std_far}")
