5edd271069da128b15af281ad3c56922085c4e17
Neeraj Kumar Vaid <neeraj.kumar.iitg@gmail.com>
2019-10-02 17:36:47 -0400

    Update decision_trees.py

--- decision_trees.py
+++ decision_trees.py
@@ -138,3 +138,41 @@ def classify(tree: DecisionTree, input: Any) -> Any:
     
     subtree = tree.subtrees[subtree_key] # Use the appropriate subtree
     return classify(subtree, input) # and use it to classify the input
+def build_tree_id3(inputs: List[Any],
+                  split_attributes: str,
+                  target_attribute: str) -> DecisionTree:
+    
+    # Count target labels
+    label_counts = Counter(getattr(input, target_attribute)
+                          for input in inputs)
+    
+    most_common_label = label_counts.most_common(1)[0][0]
+    
+    # If there's a unique label predict it
+    
+    if len(label_counts) == 1:
+        return Leaf(most_common_label)
+    
+    # If no split attributes left, return the majority label
+    if not split_attributes:
+        return Leaf(most_common_label)
+    
+    # Otherwise split by the best attribute
+    
+    def split_entropy(attribute: str) -> float:
+        """Helper function for finding the best attribute"""
+        return partition_entropy_by(inputs, attribute, target_attribute)
+    
+    best_attribute = min(split_attributes, key = split_entropy)
+    
+    partitions = partition_by(inputs, best_attribute)
+    new_attributes  = [a for a in split_attributes if a != best_attribute]
+    
+    # Recursively build the subtrees
+    subtrees = {attribute_value : build_tree_id3(subset,
+                                                new_attributes,
+                                                target_attribute)
+               for attribute_value, subset in partition.items()}
+    return Split(best_attribute, subtrees, default_value = most_common_label)
+
+
