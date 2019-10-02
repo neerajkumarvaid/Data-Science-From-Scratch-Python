3a5020f4860332620e2a6b9e2117d3fefb72cb88
Neeraj Kumar Vaid <neeraj.kumar.iitg@gmail.com>
2019-10-02 17:34:53 -0400

    Update decision_trees.py

--- decision_trees.py
+++ decision_trees.py
@@ -39,3 +39,47 @@ def partition_entropy(subsets: List[List[Any]]) -> float:
     total_count = sum(len(subset) for subset in subsets)
     return sum(data_entropy(subset) * len(subset) / total_count
               for subset in subsets)
+from typing import NamedTuple, Optional
+
+class Candidate(NamedTuple):
+    level: str
+    lang: str
+    tweets: bool
+    phd: bool
+    did_well: Optional[bool] = None # allow unlabeled data
+        
+                  #  level     lang     tweets  phd  did_well
+inputs = [Candidate('Senior', 'Java',   False, False, False),
+          Candidate('Senior', 'Java',   False, True,  False),
+          Candidate('Mid',    'Python', False, False, True),
+          Candidate('Junior', 'Python', False, False, True),
+          Candidate('Junior', 'R',      True,  False, True),
+          Candidate('Junior', 'R',      True,  True,  False),
+          Candidate('Mid',    'R',      True,  True,  True),
+          Candidate('Senior', 'Python', False, False, False),
+          Candidate('Senior', 'R',      True,  False, True),
+          Candidate('Junior', 'Python', True,  False, True),
+          Candidate('Senior', 'Python', True,  True,  True),
+          Candidate('Mid',    'Python', False, True,  True),
+          Candidate('Mid',    'Java',   True,  False, True),
+          Candidate('Junior', 'Python', False, True,  False)
+         ]
+
+from typing import Dict, TypeVar
+from collections import defaultdict
+
+T = TypeVar('T') # generic type for inputs
+
+def partition_by(inputs: List[T], attribute: str) -> Dict[Any, List[T]]:
+    """Partition the inputs into lists based on the specified attribute"""
+    partitions: Dict[Any, List[T]] = defaultdict(list)
+        
+    for input in inputs:
+        key = getattr(input, attribute) # value of the specified attribute
+        partitions[key].append(input)
+        
+    return partitions
+
+partition = partition_by(inputs, 'level')
+print(partition.keys())
+print(partition.values())
