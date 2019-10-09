345a51ee1b29d9bba2399122fe816d7d79b2a43e
Neeraj Kumar Vaid <neeraj.kumar.iitg@gmail.com>
2019-10-08 22:19:23 -0400

    Update neural_networks.py

--- neural_networks.py
+++ neural_networks.py
@@ -1,1 +1,19 @@
+def step_function(x: float) -> float:
+    return 1.0 if x >= 0 else 0.0
+
+from vector_operations import Vector, dot;
+
+def perceptron_output(weights: Vector, bias: float, x: Vector) -> float:
+    """Returns 1 if the perceptron fires, else returns 0"""
+    calculation = dot(weights, x) + bias
+    return step_function(calculation)
+    
+and_weights = [2., 2.]
+and_bias = -3.
+
+print(f"perceptron_output(and_weights, and_bias, [1, 1]) = {perceptron_output(and_weights, and_bias, [1, 1])}")
+print(f"perceptron_output(and_weights, and_bias, [0, 1]) = {perceptron_output(and_weights, and_bias, [0, 1])}")
+print(f"perceptron_output(and_weights, and_bias, [1, 0]) = {perceptron_output(and_weights, and_bias, [1, 0])}")
+print(f"perceptron_output(and_weights, and_bias, [0, 0]) = {perceptron_output(and_weights, and_bias, [0, 0])}")
+
 
