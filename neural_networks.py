7bf3c781574b7a34b291989e145535dc97a9442f
Neeraj Kumar Vaid <neeraj.kumar.iitg@gmail.com>
2019-10-08 22:21:25 -0400

    Update neural_networks.py

--- neural_networks.py
+++ neural_networks.py
@@ -145,3 +145,20 @@ for epoch in tqdm.trange(20000, desc = "neural net for xor"):
                   for layer, layer_grad in zip(network, gradients)]
         
 
+# fizz-buzz problem
+
+def fizz_buzz_encode(x: int) -> Vector:
+    if x % 15 == 0:
+        return [0,0,0,1]
+    elif x % 5 == 0:
+        return [0,0,1,0]
+    elif x % 3 == 0:
+        return [0,1,0,0]
+    else:
+        return [1,0,0,0]
+    
+print(fizz_buzz_encode(2))
+print(fizz_buzz_encode(6))
+print(fizz_buzz_encode(25))
+print(fizz_buzz_encode(45))
+
