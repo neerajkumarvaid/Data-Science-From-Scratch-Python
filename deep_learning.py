913bda0fe322831c3cd2e674acb7721f8eace6d9
Neeraj Kumar Vaid <neeraj.kumar.iitg@gmail.com>
2019-10-09 20:14:06 -0400

    Update deep_learning.py

--- deep_learning.py
+++ deep_learning.py
@@ -14,3 +14,17 @@ print(f"shape([1,2,3]) = {shape([1,2,3])}")
 print(f"shape([[1,2],[3,4],[5,6]]) = {shape([[1,2],[3,4],[5,6]])}")
 print(f"shape([[[1,2,3], [4,5,6]],[[7,8,9],[10,11,12]],[[15,16,17],[18,19,20]]]) = {shape([[[1,2,3], [4,5,6]],[[7,8,9],[10,11,12]],[[15,16,17],[18,19,20]]])}")
 
+def is_1d(tensor: Tensor) -> bool:
+    """If tensor[0] is a list, it's a higher-order tensor.
+    Otherwise, tensor is 1-dimensional (that is, a vector)."""
+    return not isinstance(tensor[0], list)
+
+print(f"is_1d([1,2,3]) = {is_1d([1,2,3])}")
+print(f"is_1d([[1,2],[3,4]]) = {is_1d([[1,2],[3,4]])}")
+
+def tensor_sum(tensor: Tensor) -> float:
+    """Sums up all the values in a tensor"""
+    if is_1d(tensor):
+        return sum(tensor)
+    else:
+        return sum(tensor_sum(tensor_i) for tensor_i in tensor)
