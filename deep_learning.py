36c977caeb4707054314e54f23bfa0fa1c296d43
Neeraj Kumar Vaid <24767040+neerajkumarvaid@users.noreply.github.com>
2019-11-10 16:14:50 -0500

    Update deep_learning.py

--- deep_learning.py
+++ deep_learning.py
@@ -62,3 +62,41 @@ import operator
 
 print("tensor_combine(operator.add, [1,2,3], [4,5,6])" +  
       f" = {tensor_combine(operator.add, [1,2,3], [4,5,6])}")
+
+from typing import Iterable, Tuple
+
+class Layer:
+    """
+    Our neural networks will be composed of Layers, each of which
+    knows how to do some computation on its inputs in the "forward"
+    direction and propagate gradients in the "backward" direction.
+    """
+    def forward(self, input):
+        """
+        Note the lack of types. We're not going to be prescriptive
+        about what kinds of inputs layers can take and what kinds
+        of outputs they can return.
+        """
+        raise NotImplementedError
+
+    def backward(self, gradient):
+        """
+        Similarly, we're not going to be prescriptive about what the
+        gradient looks like. It's up to you the user to make sure
+        that you're doing things sensibly.
+        """
+        raise NotImplementedError
+
+    def params(self) -> Iterable[Tensor]:
+        """
+        Returns the parameters of this layer. The default implementation
+        returns nothing, so that if you have a layer with no parameters
+        you don't have to implement this.
+        """
+        return ()
+
+    def grads(self) -> Iterable[Tensor]:
+        """
+        Returns the gradients, in the same order as params()
+        """
+        return ()
