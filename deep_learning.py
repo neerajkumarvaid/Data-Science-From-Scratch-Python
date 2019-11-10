193d11ed4d6a2d280853df6696d7657aac437402
Neeraj Kumar Vaid <24767040+neerajkumarvaid@users.noreply.github.com>
2019-11-10 16:18:57 -0500

    Update deep_learning.py

--- deep_learning.py
+++ deep_learning.py
@@ -317,3 +317,33 @@ class Momentum(Optimizer):
                 lambda p, u: p - self.lr * u,
                 param,
                 update)            
+
+xs = [[0., 0], [0., 1], [1., 0], [1., 1]]
+ys = [[0.], [1.], [1.], [0.]]
+    
+random.seed(0)
+    
+net = Sequential([
+        Linear(input_dim=2, output_dim=2),
+        Sigmoid(),
+        Linear(input_dim=2, output_dim=1)
+    ])
+    
+import tqdm
+    
+optimizer = GradientDescent(learning_rate=0.1)
+loss = SSE()
+    
+with tqdm.trange(3000) as t:
+    for epoch in t:
+        epoch_loss = 0.0
+    
+        for x, y in zip(xs, ys):
+            predicted = net.forward(x)
+            epoch_loss += loss.loss(predicted, y)
+            gradient = loss.gradient(predicted, y)
+            net.backward(gradient)
+    
+            optimizer.step(net)
+    
+        t.set_description(f"xor loss {epoch_loss:.3f}")            
