9829744888c085955988839fbdda384d9347d812
Neeraj Kumar Vaid <24767040+neerajkumarvaid@users.noreply.github.com>
2019-11-10 16:16:57 -0500

    Update deep_learning.py

--- deep_learning.py
+++ deep_learning.py
@@ -197,3 +197,41 @@ class Linear(Layer):
 
     def grads(self) -> Iterable[Tensor]:
         return [self.w_grad, self.b_grad]        
+
+from typing import List
+
+class Sequential(Layer):
+    """
+    A layer consisting of a sequence of other layers.
+    It's up to you to make sure that the output of each layer
+    makes sense as the input to the next layer.
+    """
+    def __init__(self, layers: List[Layer]) -> None:
+        self.layers = layers
+
+    def forward(self, input):
+        """Just forward the input through the layers in order."""
+        for layer in self.layers:
+            input = layer.forward(input)
+        return input
+
+    def backward(self, gradient):
+        """Just backpropagate the gradient through the layers in reverse."""
+        for layer in reversed(self.layers):
+            gradient = layer.backward(gradient)
+        return gradient
+
+    def params(self) -> Iterable[Tensor]:
+        """Just return the params from each layer."""
+        return (param for layer in self.layers for param in layer.params())
+
+    def grads(self) -> Iterable[Tensor]:
+        """Just return the grads from each layer."""
+        return (grad for layer in self.layers for grad in layer.grads())
+    
+xor_net = Sequential([
+    Linear(input_dim = 2, output_dim = 2),
+    Sigmoid(),
+    Linear(input_dim = 2, output_dim = 1),
+    Sigmoid()
+])    
