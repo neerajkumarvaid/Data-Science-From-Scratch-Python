297261af8e9198d39de37628e7d39a40e4576548
Neeraj Kumar Vaid <24767040+neerajkumarvaid@users.noreply.github.com>
2019-11-10 16:16:08 -0500

    Update deep_learning.py

--- deep_learning.py
+++ deep_learning.py
@@ -116,3 +116,36 @@ class Sigmoid(Layer):
         return tensor_combine(lambda sig, grad: sig * (1 - sig) * grad,
                              self.sigmoids,
                              gradient)
+
+import random
+from probability import inverse_normal_cdf
+
+def random_uniform(*dims: int) -> Tensor:
+    if len(dims) == 1:
+        return [random.random() for _ in range(dims[0])]
+    else:
+        return [random_uniform(*dims[1:]) for _ in range(dims[0])]
+    
+def random_normal(*dims: int,
+                 mean: float = 0.0,
+                 variance: float = 1.0) -> Tensor:
+    if len(dims) == 1:
+        return [mean + variance*inverse_normal_cdf(random.random()) 
+                for _ in range(dims[0])]
+    else:
+        return [random_normal(*dims[1:], mean = mean, variance = variance)
+                for _ in range(dims[0])]
+    
+print(random_normal(2,3,4))
+
+
+def random_tensor(*dims: int, init: str = 'normal') -> Tensor:
+    if init == 'normal':
+        return random_normal(*dims)
+    elif init == 'uniform':
+        return random_uniform(*dims)
+    elif init == 'xavier':
+        variance = len(dims) / sum(dims)
+        return random_normal(*dims,variance = variance)
+    else:
+        raise ValueError(f"unkown init: {init}")
