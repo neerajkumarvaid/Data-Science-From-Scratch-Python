9de7fdf61f527bfde1ab679cc6c5abef4093bf2a
Neeraj Kumar Vaid <24767040+neerajkumarvaid@users.noreply.github.com>
2019-11-10 16:16:37 -0500

    Update deep_learning.py

--- deep_learning.py
+++ deep_learning.py
@@ -149,3 +149,51 @@ def random_tensor(*dims: int, init: str = 'normal') -> Tensor:
         return random_normal(*dims,variance = variance)
     else:
         raise ValueError(f"unkown init: {init}")
+
+from vector_operations import dot
+
+class Linear(Layer):
+    def __init__(self, input_dim: int, output_dim: int, init: str = 'xavier') -> None:
+        """
+        A layer of output_dim neurons, each with input_dim weights
+        (and a bias).
+        """
+        self.input_dim = input_dim
+        self.output_dim = output_dim
+
+        # self.w[o] is the weights for the o-th neuron
+        self.w = random_tensor(output_dim, input_dim, init=init)
+
+        # self.b[o] is the bias term for the o-th neuron
+        self.b = random_tensor(output_dim, init=init)
+
+    def forward(self, input: Tensor) -> Tensor:
+        # Save the input to use in the backward pass.
+        self.input = input
+
+        # Return the vector of neuron outputs.
+        return [dot(input, self.w[o]) + self.b[o]
+                for o in range(self.output_dim)]
+
+    def backward(self, gradient: Tensor) -> Tensor:
+        # Each b[o] gets added to output[o], which means
+        # the gradient of b is the same as the output gradient.
+        self.b_grad = gradient
+
+        # Each w[o][i] multiplies input[i] and gets added to output[o].
+        # So its gradient is input[i] * gradient[o].
+        self.w_grad = [[self.input[i] * gradient[o]
+                        for i in range(self.input_dim)]
+                       for o in range(self.output_dim)]
+
+        # Each input[i] multiplies every w[o][i] and gets added to every
+        # output[o]. So its gradient is the sum of w[o][i] * gradient[o]
+        # across all the outputs.
+        return [sum(self.w[o][i] * gradient[o] for o in range(self.output_dim))
+                for i in range(self.input_dim)]
+
+    def params(self) -> Iterable[Tensor]:
+        return [self.w, self.b]
+
+    def grads(self) -> Iterable[Tensor]:
+        return [self.w_grad, self.b_grad]        
