6a577454b2ff5d86cb6f60375ecd2d4cfb720312
Neeraj Kumar Vaid <24767040+neerajkumarvaid@users.noreply.github.com>
2020-01-01 19:28:30 -0500

    Create clustering.py

---
+++ clustering.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan  1 07:27:33 2020
+@author: Neeraj
+Description: This file contains an impelementation fo k-means clustering from scratch in Python.
+Reference: Chapter 20 Clustering
+"""
