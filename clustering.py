9eb9397f85625566e213673ef4fb53bb98f802aa
Neeraj Kumar Vaid <24767040+neerajkumarvaid@users.noreply.github.com>
2020-01-02 19:04:36 -0500

    Update clustering.py

--- clustering.py
+++ clustering.py
@@ -2,7 +2,7 @@
 """
 Created on Wed Jan  1 07:27:33 2020
 @author: Neeraj
-Description: This file contains an impelementation fo k-means clustering from scratch in Python.
+Description: This file contains an impelementation of k-means clustering from scratch in Python.
 Reference: Chapter 20 Clustering
 """
 
