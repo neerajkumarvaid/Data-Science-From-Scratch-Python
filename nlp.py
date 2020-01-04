422ce3159df058e5cc6496201ae25c10ca832436
Neeraj Kumar Vaid <24767040+neerajkumarvaid@users.noreply.github.com>
2020-01-03 19:16:30 -0500

    Update nlp.py

--- nlp.py
+++ nlp.py
@@ -171,4 +171,43 @@ def expand(grammar: Grammar, tokens: List[str]) -> List[str]:
 def generate_sentence(grammar: Grammar) -> List[str]:
     return expand(grammar, ["_S"])         
          
-         
+# Gibbs Sampling
+
+from typing import Tuple
+import random
+
+def roll_a_die() -> int:
+    return random.choice([1, 2, 3, 4, 5, 6])
+
+def direct_sample() -> Tuple[int, int]:
+    d1 = roll_a_die()
+    d2 = roll_a_die()
+    return d1, d1 + d2
+
+def random_y_given_x(x: int) -> int:
+    """equally likely to be x + 1, x + 2, ... , x + 6"""
+    return x + roll_a_die()
+
+def random_x_given_y(y: int) -> int:
+    if y <= 7:
+        # if the total is 7 or less, the first die is equally likely to be
+        # 1, 2, ..., (total - 1)
+        return random.randrange(1, y)
+    else:
+        # if the total is 7 or more, the first die is equally likely to be
+        # (total - 6), (total - 5), ..., 6
+        return random.randrange(y - 6, 7)
+    
+def gibbs_sample(num_iters: int = 100) -> Tuple[int, int]:
+    x, y = 1, 2 # doesn't really matter
+    for _ in range(num_iters):
+        x = random_x_given_y(y)
+        y = random_y_given_x(x)
+    return x, y
+
+def compare_distributions(num_samples: int = 1000) -> Dict[int, List[int]]:
+    counts = defaultdict(lambda: [0, 0])
+    for _ in range(num_samples):
+        counts[gibbs_sample()][0] += 1
+        counts[direct_sample()][1] += 1
+    return counts         
