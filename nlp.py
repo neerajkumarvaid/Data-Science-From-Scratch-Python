bf2dfef9d74d886e811407f0d404f88dcc5c8c64
Neeraj Kumar Vaid <24767040+neerajkumarvaid@users.noreply.github.com>
2020-01-03 19:14:03 -0500

    Update nlp.py

--- nlp.py
+++ nlp.py
@@ -6,3 +6,36 @@ Description: This file contains an impelementation of natural language processin
 topic modeling, etc. from scratch in Python. It also contains code to generate word clouds.
 Reference: Chapter 21 Natural Language Processing
 """
+data = [ ("big data", 100, 15), ("Hadoop", 95, 25), ("Python", 75, 50),
+         ("R", 50, 40), ("machine learning", 80, 20), ("statistics", 20, 60),
+         ("data science", 60, 70), ("analytics", 90, 3),
+         ("team player", 85, 85), ("dynamic", 2, 90), ("synergies", 70, 0),
+         ("actionable insights", 40, 30), ("think out of the box", 45, 10),
+         ("self-starter", 30, 50), ("customer focus", 65, 15),
+         ("thought leadership", 35, 35)]
+
+# How to creat a word cloud?
+
+from wordcloud import WordCloud, STOPWORDS 
+import matplotlib.pyplot as plt 
+
+stopwords = set(STOPWORDS) 
+
+words = [t[0] for t in data]
+
+# convert the list of words into a combined string
+comment_words = ' '
+for word in words:
+    comment_words = comment_words + word + ' '
+
+# create the word cloud
+wordcloud = WordCloud(width = 800, height = 800, 
+                background_color ='white', 
+                stopwords = stopwords, 
+                min_font_size = 10).generate(comment_words)
+
+# plot the WordCloud image                        
+plt.figure(figsize = (8, 8), facecolor = None) 
+plt.imshow(wordcloud) 
+plt.axis("off") 
+plt.tight_layout(pad = 0)
