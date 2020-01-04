862557f081dc09487fe01329b14bd18f83ec04fe
Neeraj Kumar Vaid <24767040+neerajkumarvaid@users.noreply.github.com>
2020-01-03 19:13:19 -0500

    Create nlp.py

---
+++ nlp.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Friday Jan  03 07:11:33 2020
+@author: Neeraj
+Description: This file contains an impelementation of natural language processing algorithms including n-grams, gibbs sampling, 
+topic modeling, etc. from scratch in Python. It also contains code to generate word clouds.
+Reference: Chapter 21 Natural Language Processing
+"""
