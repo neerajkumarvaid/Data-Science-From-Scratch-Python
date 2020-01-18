3dcd108f5f1334c588c1a4feb15f92a990034d88
Neeraj Kumar Vaid <24767040+neerajkumarvaid@users.noreply.github.com>
2020-01-17 23:11:55 -0500

    Update network_analysis.py

--- network_analysis.py
+++ network_analysis.py
@@ -19,3 +19,16 @@ users = [User(0, "Hero"), User(1,"Dunn"), User(2, "Sue"), User(3,"Chi"), User(4,
         User(6, "Hicks"), User(7,"Devin"), User(8,"Kate"), User(9,"Klein")]
 
 friend_pairs = [(0,1),(0,2),(1,2),(1,3),(2,3),(3,4),(4,5),(5,6),(5,7),(6,8),(7,8),(8,9)]
+
+from typing import Dict, List
+
+# Type aliases for keeping track if Friendships
+Friendships = Dict[int, List[int]]
+
+friendships: Friendships = {user.id: [] for user in users}
+    
+for i, j in friend_pairs:
+    friendships[i].append(j)
+    friendships[j].append(i)
+    
+#friendships
