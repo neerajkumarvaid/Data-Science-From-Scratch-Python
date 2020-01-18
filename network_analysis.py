03f3f296485ec1819ee128e061cb18c1eb0e4eee
Neeraj Kumar Vaid <24767040+neerajkumarvaid@users.noreply.github.com>
2020-01-17 23:13:05 -0500

    Update network_analysis.py

--- network_analysis.py
+++ network_analysis.py
@@ -104,3 +104,17 @@ def farness(user_id: int) -> float:
 
 closeness_centrality = {user.id: 1/farness(user.id) for user in users}
 #closeness_centrality
+
+from matrix_operations import Matrix, make_matrix, shape
+
+def matrix_times_matrix(m1: Matrix, m2: Matrix) -> Matrix:
+    nr1, nc1 = shape(m1)
+    nr2, nc2 = shape(m2)
+    
+    assert nc1 == nr2, "must have (# o f columns in m1) == (# of rows in m2)"
+    
+    def entry_fn(i: int, j: int) -> float:
+        """dot product of i-th row of m1 with the j-th column of m2"""
+        return sum(m1[i][k]*m2[k][j] for k in range(nc1))
+    
+    return make_matrix(nr1,nc2, entry_fn)
