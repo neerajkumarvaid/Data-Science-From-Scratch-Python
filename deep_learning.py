041a01de03a79b7f9eff939e69abb50b13ea8074
Neeraj Kumar Vaid <24767040+neerajkumarvaid@users.noreply.github.com>
2020-01-21 14:49:25 -0500

    Update deep_learning.py

--- deep_learning.py
+++ deep_learning.py
@@ -1,7 +1,7 @@
 """
 Created on Tue Nov  12 11:37:37 2019
 @author: Neeraj
-Description: Implementation deep neural networks with various loss fucntions and optimization techniques including 
+Description: Implementation of deep neural networks with various loss fucntions and optimization techniques including 
 network regulation using dropout from scratch in Python. 
 Reference: Chapter 19 : Deep Learning
 """
