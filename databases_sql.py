8d515f5a4755352b08d6f1c36a99a29354a27a80
Neeraj Kumar Vaid <24767040+neerajkumarvaid@users.noreply.github.com>
2020-01-26 16:45:13 -0500

    Update databases_sql.py

--- databases_sql.py
+++ databases_sql.py
@@ -193,3 +193,16 @@ class Table:
                                  [None for c in additional_columns])
                 
         return join_table  
+
+users = Table(['user_id', 'name', 'num_friends'], [int, str, int])
+users.insert([0, "Hero", 0])
+users.insert([1, "Dunn", 2])
+users.insert([2, "Sue", 3])
+users.insert([3, "Chi", 3])
+users.insert([4, "Thor", 3])
+users.insert([5, "Clive", 2])
+users.insert([6, "Hicks", 3])
+users.insert([7, "Devin", 2])
+users.insert([8, "Kate", 2])
+users.insert([9, "Klein", 3])
+users.insert([10, "Jen", 1])
