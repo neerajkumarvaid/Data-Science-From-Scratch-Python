c5219feeaa8b3d113af0106c5db489083a0b76ed
Neeraj Kumar Vaid <24767040+neerajkumarvaid@users.noreply.github.com>
2020-01-26 16:53:11 -0500

    Create MapReduce.py

---
+++ MapReduce.py
@@ -0,0 +1,10 @@
+"""
+Created on Sun Jan 26 16:51:40 2020
+
+@author: Neeraj
+
+Description: An impelementation of mapper and reducer functions with a few
+examples in Python.
+Reference: Chapter 25 MapReduce
+"""
+
