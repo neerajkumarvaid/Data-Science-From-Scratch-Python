6265beecc77b64137bf9cd873108077c3d585a0d
Neeraj Kumar Vaid <24767040+neerajkumarvaid@users.noreply.github.com>
2020-01-26 16:49:50 -0500

    Update databases_sql.py

--- databases_sql.py
+++ databases_sql.py
@@ -3,8 +3,7 @@ Created on Sun Jan 26 16:40:04 2020
 
 @author: Neeraj
 
-Description: This file contains an impelementation of basic SQL operations
-in Python.
+Description: This file contains an impelementation of basic SQL operations in Python.
 Reference: Chapter 24 Databse and SQL
 """
 users = [[0, "Hero", 0],
